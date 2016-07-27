fe58a519cc7c3b77ccbe666748ef4a1a1c4fc6a2
kyle nunery <kyle.nunery@gmail.com>
2016-07-27 18:03:24 -0400

    add Ping controller and cleanup files

---
+++ src/asp-net-core-swagger-example/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_swagger_example.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
